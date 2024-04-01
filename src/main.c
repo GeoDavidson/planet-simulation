c1f79dfbdc5e967b886e876844fae64597ec3f49
George Davidson <georgedavidson392@gmail.com>
2024-04-01 17:40:02 -0400

    gravity

--- src/main.c
+++ src/main.c
@@ -2,23 +2,78 @@
 #include <raylib.h>
 #include <stdio.h>
 
+double G = 6.67430e+1; // 6.674e-11
+
+typedef struct {
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 acceleration;
+    float mass;
+} Body;
+
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int winWidth = 800;
+    const int winHeight = 450;
 
-    int circleX = 400;
-    int circleY = 225;
+    Body body1 = {{winWidth / 3, winHeight / 2}, {0, 4}, {0, 0}, 42};
+    Body body2 = {{winWidth / 2, winHeight / 2}, {0, -4}, {0, 0}, 42};
 
-    InitWindow(screenWidth, screenHeight, "basic window");
+    float distance = sqrt(pow((body2.position.x - body1.position.x), 2) + pow((body2.position.y - body1.position.y), 2));
+    float force = G * body1.mass * body2.mass / pow(distance, 2);
+    float angle = atan2(body2.position.y - body1.position.y, body2.position.x - body1.position.x);
+    float forceX = force * cos(angle);
+    float forceY = force * sin(angle);
+
+    body1.acceleration.x = forceX / body1.mass;
+    body1.acceleration.y = forceY / body1.mass;
+
+    body1.velocity.x = body1.velocity.x + body1.acceleration.x;
+    body1.velocity.y = body1.velocity.y + body1.acceleration.y;
+
+    body1.position.x = body1.position.x + body1.velocity.x;
+    body1.position.y = body1.position.y + body1.velocity.y;
+
+    InitWindow(winWidth, winHeight, "basic window");
 
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false) {
+        distance = sqrt(pow((body2.position.x - body1.position.x), 2) + pow((body2.position.y - body1.position.y), 2));
+        force = G * body1.mass * body2.mass / pow(distance, 2);
+        angle = atan2(body2.position.y - body1.position.y, body2.position.x - body1.position.x);
+        forceX = force * cos(angle);
+        forceY = force * sin(angle);
+
+        body1.acceleration.x = forceX / body1.mass;
+        body1.acceleration.y = forceY / body1.mass;
+
+        body1.velocity.x = body1.velocity.x + body1.acceleration.x;
+        body1.velocity.y = body1.velocity.y + body1.acceleration.y;
+
+        body1.position.x = body1.position.x + body1.velocity.x;
+        body1.position.y = body1.position.y + body1.velocity.y;
+
+        distance = sqrt(pow((body1.position.x - body2.position.x), 2) + pow((body1.position.y - body2.position.y), 2));
+        force = G * body2.mass * body1.mass / pow(distance, 2);
+        angle = atan2(body1.position.y - body2.position.y, body1.position.x - body2.position.x);
+        forceX = force * cos(angle);
+        forceY = force * sin(angle);
+
+        body2.acceleration.x = forceX / body2.mass;
+        body2.acceleration.y = forceY / body2.mass;
+
+        body2.velocity.x = body2.velocity.x + body2.acceleration.x;
+        body2.velocity.y = body2.velocity.y + body2.acceleration.y;
+
+        body2.position.x = body2.position.x + body2.velocity.x;
+        body2.position.y = body2.position.y + body2.velocity.y;
+
         BeginDrawing();
 
         ClearBackground(WHITE);
 
-        DrawCircle(circleX + 125 * sin(GetTime()), circleY + 125 * cos(GetTime()), 25, BLACK);
+        DrawCircle(body1.position.x, body1.position.y, 25, BLACK);
+        DrawCircle(body2.position.x, body2.position.y, 50, BLACK);
 
         EndDrawing();
     }
