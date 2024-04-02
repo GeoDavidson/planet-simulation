936e907e12b911ac197b558f3228ccf7bc2b847c
George Davidson <georgedavidson392@gmail.com>
2024-04-01 22:33:26 -0400

    added particle trails

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 #include <raymath.h>
 #include <stdio.h>
 
-double G = 6.674e-11;
+#define G 6.674e-11
+#define MAX_PARTICLES 1000
 
 typedef struct {
     Vector2 position;
@@ -11,6 +12,10 @@ typedef struct {
     Vector2 acceleration;
     float mass;
 } Body;
+
+typedef struct {
+    Vector2 position;
+} Particle;
 
 Vector2 calculateForce(Body body1, Body body2) {
     Vector2 force = {};
@@ -29,6 +34,30 @@ int main() {
     Body body1 = {{winWidth / 3, winHeight / 2}, {0, 1}, {0, 0}, 500000000000};
     Body body2 = {{winWidth / 2, winHeight / 2}, {0, 0}, {0, 0}, 5000000000000};
     Body body3 = {{winWidth / 1.5, winHeight / 2}, {0, -1}, {0, 0}, 5000000000000};
+
+    Particle particles1[MAX_PARTICLES];
+    int particleIndex1 = 0;
+
+    for (int i = 0; i < MAX_PARTICLES; i++) {
+        particles1[i].position.x = body1.position.x;
+        particles1[i].position.y = body1.position.y;
+    }
+
+    Particle particles2[MAX_PARTICLES];
+    int particleIndex2 = 0;
+
+    for (int i = 0; i < MAX_PARTICLES; i++) {
+        particles2[i].position.x = body2.position.x;
+        particles2[i].position.y = body2.position.y;
+    }
+
+    Particle particles3[MAX_PARTICLES];
+    int particleIndex3 = 0;
+
+    for (int i = 0; i < MAX_PARTICLES; i++) {
+        particles3[i].position.x = body3.position.x;
+        particles3[i].position.y = body3.position.y;
+    }
 
     Vector2 force = {0.0f, 0.0f};
 
@@ -120,13 +149,36 @@ int main() {
         body3.position.x += body3.velocity.x;
         body3.position.y += body3.velocity.y;
 
+        particles1[particleIndex1].position.x = body1.position.x;
+        particles1[particleIndex1].position.y = body1.position.y;
+
+        particleIndex1 = (particleIndex1 + 1) % MAX_PARTICLES;
+
+        particles2[particleIndex2].position.x = body2.position.x;
+        particles2[particleIndex2].position.y = body2.position.y;
+
+        particleIndex2 = (particleIndex2 + 1) % MAX_PARTICLES;
+
+        particles3[particleIndex3].position.x = body3.position.x;
+        particles3[particleIndex3].position.y = body3.position.y;
+
+        particleIndex3 = (particleIndex3 + 1) % MAX_PARTICLES;
+
         BeginDrawing();
 
         ClearBackground(WHITE);
 
+        for (int i = 0; i < MAX_PARTICLES; i++) {
+            DrawCircle((particles1[i].position.x + mouseOffsetX) * zoom + winWidth / 2, (particles1[i].position.y + mouseOffsetY) * zoom + winHeight / 2, 5 * zoom, LIGHTGRAY);
+            DrawCircle((particles2[i].position.x + mouseOffsetX) * zoom + winWidth / 2, (particles2[i].position.y + mouseOffsetY) * zoom + winHeight / 2, 5 * zoom, LIGHTGRAY);
+            DrawCircle((particles3[i].position.x + mouseOffsetX) * zoom + winWidth / 2, (particles3[i].position.y + mouseOffsetY) * zoom + winHeight / 2, 5 * zoom, LIGHTGRAY);
+        }
+
         DrawCircle((body1.position.x + mouseOffsetX) * zoom + winWidth / 2, (body1.position.y + mouseOffsetY) * zoom + winHeight / 2, 25 * zoom, BLACK);
         DrawCircle((body2.position.x + mouseOffsetX) * zoom + winWidth / 2, (body2.position.y + mouseOffsetY) * zoom + winHeight / 2, 25 * zoom, BLACK);
         DrawCircle((body3.position.x + mouseOffsetX) * zoom + winWidth / 2, (body3.position.y + mouseOffsetY) * zoom + winHeight / 2, 25 * zoom, BLACK);
+
+        DrawText(TextFormat("%d", GetFPS()), 5, 5, 25, BLACK);
 
         EndDrawing();
     }
