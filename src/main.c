3b1e461cbafb0a5d6094f8309d71446cba5bb374
George Davidson <georgedavidson392@gmail.com>
2024-04-03 21:53:22 -0400

    refactor

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 
 #define G 6.674e-11
 #define MAX_PARTICLES 1000
+#define MAX_BODIES 3
 
 typedef struct {
     Vector2 position;
@@ -31,32 +32,34 @@ int main() {
     const int winWidth = 800;
     const int winHeight = 450;
 
-    Body body1 = {{winWidth / 3, winHeight / 2}, {0, 1}, {0, 0}, 500000000000};
-    Body body2 = {{winWidth / 2, winHeight / 2}, {0, 0}, {0, 0}, 5000000000000};
-    Body body3 = {{winWidth / 1.5, winHeight / 2}, {0, -1}, {0, 0}, 5000000000000};
+    Body bodies[MAX_BODIES] = {
+        {{winWidth / 3, winHeight / 2}, {0, 1}, {0, 0}, 500000000000},
+        {{winWidth / 2, winHeight / 2}, {0, 0}, {0, 0}, 5000000000000},
+        {{winWidth / 1.5, winHeight / 2}, {0, -1}, {0, 0}, 5000000000000}
+    };
 
     Particle particles1[MAX_PARTICLES];
     int particleIndex1 = 0;
 
     for (int i = 0; i < MAX_PARTICLES; i++) {
-        particles1[i].position.x = body1.position.x;
-        particles1[i].position.y = body1.position.y;
+        particles1[i].position.x = bodies[0].position.x;
+        particles1[i].position.y = bodies[0].position.y;
     }
 
     Particle particles2[MAX_PARTICLES];
     int particleIndex2 = 0;
 
     for (int i = 0; i < MAX_PARTICLES; i++) {
-        particles2[i].position.x = body2.position.x;
-        particles2[i].position.y = body2.position.y;
+        particles2[i].position.x = bodies[1].position.x;
+        particles2[i].position.y = bodies[1].position.y;
     }
 
     Particle particles3[MAX_PARTICLES];
     int particleIndex3 = 0;
 
     for (int i = 0; i < MAX_PARTICLES; i++) {
-        particles3[i].position.x = body3.position.x;
-        particles3[i].position.y = body3.position.y;
+        particles3[i].position.x = bodies[2].position.x;
+        particles3[i].position.y = bodies[2].position.y;
     }
 
     Vector2 force = {0.0f, 0.0f};
@@ -83,84 +86,35 @@ int main() {
         lastMousePosX = GetMousePosition().x;
         lastMousePosY = GetMousePosition().y;
 
-        force = calculateForce(body1, body2);
+        for (int i = 0; i < MAX_BODIES; i++) {
+            for (int j = 0; j < MAX_BODIES; j++) {
+                if (i != j) {
+                    force = calculateForce(bodies[i], bodies[j]);
 
-        body1.acceleration.x = force.x / body1.mass;
-        body1.acceleration.y = force.y / body1.mass;
+                    bodies[i].acceleration.x = force.x / bodies[i].mass;
+                    bodies[i].acceleration.y = force.y / bodies[i].mass;
 
-        body1.velocity.x += body1.acceleration.x;
-        body1.velocity.y += body1.acceleration.y;
+                    bodies[i].velocity.x += bodies[i].acceleration.x;
+                    bodies[i].velocity.y += bodies[i].acceleration.y;
 
-        body1.position.x += body1.velocity.x;
-        body1.position.y += body1.velocity.y;
+                    bodies[i].position.x += bodies[i].velocity.x;
+                    bodies[i].position.y += bodies[i].velocity.y;
+                }
+            }
+        }
 
-        force = calculateForce(body1, body3);
-
-        body1.acceleration.x = force.x / body1.mass;
-        body1.acceleration.y = force.y / body1.mass;
-
-        body1.velocity.x += body1.acceleration.x;
-        body1.velocity.y += body1.acceleration.y;
-
-        body1.position.x += body1.velocity.x;
-        body1.position.y += body1.velocity.y;
-
-        force = calculateForce(body2, body1);
-
-        body2.acceleration.x = force.x / body2.mass;
-        body2.acceleration.y = force.y / body2.mass;
-
-        body2.velocity.x += body2.acceleration.x;
-        body2.velocity.y += body2.acceleration.y;
-
-        body2.position.x += body2.velocity.x;
-        body2.position.y += body2.velocity.y;
-
-        force = calculateForce(body2, body3);
-
-        body2.acceleration.x = force.x / body2.mass;
-        body2.acceleration.y = force.y / body2.mass;
-
-        body2.velocity.x += body2.acceleration.x;
-        body2.velocity.y += body2.acceleration.y;
-
-        body2.position.x += body2.velocity.x;
-        body2.position.y += body2.velocity.y;
-
-        force = calculateForce(body3, body1);
-
-        body3.acceleration.x = force.x / body3.mass;
-        body3.acceleration.y = force.y / body3.mass;
-
-        body3.velocity.x += body3.acceleration.x;
-        body3.velocity.y += body3.acceleration.y;
-
-        body3.position.x += body3.velocity.x;
-        body3.position.y += body3.velocity.y;
-
-        force = calculateForce(body3, body2);
-
-        body3.acceleration.x = force.x / body3.mass;
-        body3.acceleration.y = force.y / body3.mass;
-
-        body3.velocity.x += body3.acceleration.x;
-        body3.velocity.y += body3.acceleration.y;
-
-        body3.position.x += body3.velocity.x;
-        body3.position.y += body3.velocity.y;
-
-        particles1[particleIndex1].position.x = body1.position.x;
-        particles1[particleIndex1].position.y = body1.position.y;
+        particles1[particleIndex1].position.x = bodies[0].position.x;
+        particles1[particleIndex1].position.y = bodies[0].position.y;
 
         particleIndex1 = (particleIndex1 + 1) % MAX_PARTICLES;
 
-        particles2[particleIndex2].position.x = body2.position.x;
-        particles2[particleIndex2].position.y = body2.position.y;
+        particles2[particleIndex2].position.x = bodies[1].position.x;
+        particles2[particleIndex2].position.y = bodies[1].position.y;
 
         particleIndex2 = (particleIndex2 + 1) % MAX_PARTICLES;
 
-        particles3[particleIndex3].position.x = body3.position.x;
-        particles3[particleIndex3].position.y = body3.position.y;
+        particles3[particleIndex3].position.x = bodies[2].position.x;
+        particles3[particleIndex3].position.y = bodies[2].position.y;
 
         particleIndex3 = (particleIndex3 + 1) % MAX_PARTICLES;
 
@@ -174,9 +128,9 @@ int main() {
             DrawCircle((particles3[i].position.x + mouseOffsetX) * zoom + winWidth / 2, (particles3[i].position.y + mouseOffsetY) * zoom + winHeight / 2, 5 * zoom, LIGHTGRAY);
         }
 
-        DrawCircle((body1.position.x + mouseOffsetX) * zoom + winWidth / 2, (body1.position.y + mouseOffsetY) * zoom + winHeight / 2, 25 * zoom, BLACK);
-        DrawCircle((body2.position.x + mouseOffsetX) * zoom + winWidth / 2, (body2.position.y + mouseOffsetY) * zoom + winHeight / 2, 25 * zoom, BLACK);
-        DrawCircle((body3.position.x + mouseOffsetX) * zoom + winWidth / 2, (body3.position.y + mouseOffsetY) * zoom + winHeight / 2, 25 * zoom, BLACK);
+        DrawCircle((bodies[0].position.x + mouseOffsetX) * zoom + winWidth / 2, (bodies[0].position.y + mouseOffsetY) * zoom + winHeight / 2, 25 * zoom, BLACK);
+        DrawCircle((bodies[1].position.x + mouseOffsetX) * zoom + winWidth / 2, (bodies[1].position.y + mouseOffsetY) * zoom + winHeight / 2, 25 * zoom, BLACK);
+        DrawCircle((bodies[2].position.x + mouseOffsetX) * zoom + winWidth / 2, (bodies[2].position.y + mouseOffsetY) * zoom + winHeight / 2, 25 * zoom, BLACK);
 
         DrawText(TextFormat("%d", GetFPS()), 5, 5, 25, BLACK);
 
